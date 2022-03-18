2fdfbe53fe18005746588fee876e7ed96ca4f61f
Jeremy Whitlock <jcwhitlock@google.com>
2022-03-18 11:29:25 -0400

    Updating for work

--- .emacs.d/customizations/file-associations.el
+++ .emacs.d/customizations/file-associations.el
@@ -1,5 +1,6 @@
 ;;; file-associations.el --- File associations for Emacs -*- lexical-binding: t; -*-
 
+(add-to-list 'auto-mode-alist '("BUILD$" .  bazel-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" .  json-mode))
 (add-to-list 'auto-mode-alist '(".jshintrc$" .  json-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
